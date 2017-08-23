5686248d1d5259a7377e1d7aae11e8200cae8bec
deaundrapeek <jingyufanclub@gmail.com>
2017-08-22 23:35:33 -0400

    test lazy load for ios

--- _includes/main.js
+++ _includes/main.js
@@ -7,17 +7,17 @@ window.onload = function() {
     threshold: 1
   };
   let observer;
-  
-  if ('IntersectionObserver' in window) {
+
+  // if ('IntersectionObserver' in window) {
     observer = new IntersectionObserver(handleIntersect, options);
     images.forEach(image => {
       observer.observe(image);
     });
-  } else {
-    images.forEach(image => {
-      image.src = image.dataset.src;
-    })
-  }
+  // } else {
+  //   images.forEach(image => {
+  //     image.src = image.dataset.src;
+  //   })
+  // }
 
   function handleIntersect(entries) {
     entries.forEach(entry => {
