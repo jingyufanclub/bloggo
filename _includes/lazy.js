a276d033bc5e5b2d4ed633cd6add9c7b8b160903
deaundrapeek <jingyufanclub@gmail.com>
2017-08-23 00:27:25 -0400

    intersect observer does not work in chrome ios

--- _includes/lazy.js
+++ _includes/lazy.js
@@ -1,32 +1,34 @@
 window.onload = function() {
+  
   console.log("meow!");
   const images = document.querySelectorAll(".lazy-image");
   const options = {
     root: null,
     rootMargin: "0px",
-    threshold: 1
+    threshold: 0.3
   };
   let observer;
 
-  // if ('IntersectionObserver' in window) {
+  if ('IntersectionObserver' in window) {
     observer = new IntersectionObserver(handleIntersect, options);
     images.forEach(image => {
       observer.observe(image);
     });
-  // } else {
-  //   images.forEach(image => {
-  //     image.src = image.dataset.src;
-  //   })
-  // }
+  } else {
+    images.forEach(image => {
+      image.src = image.dataset.src;
+    });
+  }
 
   function handleIntersect(entries) {
     entries.forEach(entry => {
       if (entry.intersectionRatio > 0) {
-        let img = entry.target
-        observer.unobserve(img);
+        let img = entry.target;
         img.src = img.dataset.src;
         img.classList.add("fade-in");
+        observer.unobserve(img);
       }
-    })
+    });
   }
+
 }
